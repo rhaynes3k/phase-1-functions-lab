4e7eedcecc5dc68d4d08239a2348b75847c53794
rhaynes3k <rhaynes3k@gmail.com>
2021-11-06 17:17:06 -0500

    commit tree 7c3f776bf05b0f3584792a0492307bda54dfbc3e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+  if (num > 42){
+    return num - 42
+  } else {
+    return 42 - num
+  }
+}
+
+function distanceFromHqInFeet(num){
+  return distanceFromHqInBlocks(num) * 264
+}
+
+function distanceTravelledInFeet(strt, dest){
+  if (strt > dest){
+    return (strt - dest) * 264
+  }else {
+    return (dest - strt) * 264
+  }
+}
+
+function calculatesFarePrice(strt, dest) {
+  let dist = distanceTravelledInFeet(strt, dest)
+  if (dist <= 400){
+    return 0
+  }else if (dist > 400 && dist <= 2000){
+    return (dist - 400) * .02
+  }else if (dist > 2000 && dist <= 2500) {
+    return 25
+  }else {
+    return 'cannot travel that far'
+  }
+}
